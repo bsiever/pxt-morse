327da8717763ead99c2963b02a1577b9ee202cb0
Bill Siever <bsiever@gmail.com>
2023-06-20 12:49:55 -0500

    Added encode string.

--- morse.ts
+++ morse.ts
@@ -2,8 +2,8 @@
 
 //% color=#cf64ed
 //% icon="\uf0a7"
-//% block="Morse Code Decoder"
-namespace morseDecoder {
+//% block="Morse Code Utilities"
+namespace morse {
 
 export enum Space {
     //% block="between letters"
@@ -16,7 +16,8 @@ export enum Space {
     
 const DOT = 1
 const DASH = 2
-    
+const DOT_CHAR = "."
+const DASH_CHAR = "-"  
 const morseTree = "?ETIANMSURWDKGOHVF?L?PJBXCYZQ??54?3???2??+????16=/?????7???8?90?"
 const MAX_STATE = morseTree.length-1
 const START_STATE = 0
@@ -87,8 +88,67 @@ export function onCodeSelected(handler: (code: string) => void) {
 
 //% blockId=onError block="on error"
 //% weight=50 
-    export function onError(handler: () => void) {
+export function onError(handler: () => void) {
     errorHandler = handler
 }
 
+// Find the code for a single character 
+// Length of string must be exactly 1
+// Returns null for invalid characters
+//% blockId=encodeCharacter block="encode $character"
+//% weight=50 
+export function encodeChar(character: string) : string {
+    if (character.length != 1) {
+        return null
+    }
+    // Upper case it
+    character = character.toUpperCase()
+    let start = morseTree.indexOf(character.charAt(0))
+    if(start==-1) {
+        return ERROR_CODE
+    }
+    let code = ""
+    while(start>0) {
+        // Odds represent dots
+        if(start%2==1) {
+            code = DOT_CHAR + code
+        } else {
+            code = DASH_CHAR + code
+        }
+        start = Math.idiv(start-1, 2)
+    }
+    return code
+}
+
+export function encode(characters: string) : string {
+    let result = ""
+    for(let c of characters) {
+        switch(c) {
+            case " ":
+                result += c
+            break;
+            case "\n":
+                result += c
+            break;
+            default: 
+                result += encodeChar(c)            
+        }
+    }
+    return result
+}
+
+
+
+/*
+ * TODO:
+ *  keyDown
+ *  keyUp
+ *  setDotTime (default based on Morse)
+ *      Include fraction of error time allowed (50% default?)
+ *      Max?
+
+ *  encode(char)
+ * 
+ *  Change Callback to provide / track dots/dashes and always be called.
+ */
 }
